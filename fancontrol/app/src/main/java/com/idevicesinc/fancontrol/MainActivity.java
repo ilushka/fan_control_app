17d4190b9c6d7dafc666403eae0d8c6f6d09fd6a
ilya moskovko <ilya@moskovko.com>
2019-03-19 23:24:02 -0700

    settings activity now initializes widgets from preferences. it will, also, store theme settings to preferences and will send them over udp. also, added drawer.

--- fancontrol/app/src/main/java/com/idevicesinc/fancontrol/MainActivity.java
+++ fancontrol/app/src/main/java/com/idevicesinc/fancontrol/MainActivity.java
@@ -1,28 +1,21 @@
 package com.idevicesinc.fancontrol;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.io.IOException;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Map;
-
-import static android.preference.PreferenceManager.getDefaultSharedPreferences;
-import static com.idevicesinc.fancontrol.R.drawable.ocean;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     private SharedPreferences sharedPreferences;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +47,21 @@ public class MainActivity extends AppCompatActivity {
         ImageView wind = (ImageView) findViewById(R.id.wind_button);
         wind.setOnClickListener(clickListener);
         wind.setOnLongClickListener(longClickListener);
+
+        // drawer stuff
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+
+
+                        return true;
+                    }
+                });
     }
 
     void startSettingsActivity(int id) {
@@ -63,37 +71,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void sendThemeFromPreferences(int theme) {
-        String sprayPref = null, fanPref = null, colorPref = null;
-        int sprayDefault = 0, fanDefault = 0, colorDefault = 0;
-        switch (theme) {
-            case R.id.ocean_button:
-                sprayPref = SettingsActivity.PREF_OCEAN_SPRAY;
-                fanPref = SettingsActivity.PREF_OCEAN_FAN;
-                colorPref = SettingsActivity.PREF_OCEAN_COLOR;
-                sprayDefault = SettingsActivity.DEFAULT_OCEAN_SPRAY;
-                fanDefault = SettingsActivity.DEFAULT_OCEAN_FAN;
-                colorDefault = SettingsActivity.DEFAULT_OCEAN_COLOR;
-                break;
-            case R.id.mountains_button:
-                sprayPref = SettingsActivity.PREF_MOUNTAINS_SPRAY;
-                fanPref = SettingsActivity.PREF_MOUNTAINS_FAN;
-                colorPref = SettingsActivity.PREF_MOUNTAINS_COLOR;
-                sprayDefault = SettingsActivity.DEFAULT_MOUNTAINS_SPRAY;
-                fanDefault = SettingsActivity.DEFAULT_MOUNTAINS_FAN;
-                colorDefault = SettingsActivity.DEFAULT_MOUNTAINS_COLOR;
-                break;
-            case R.id.wind_button:
-                sprayPref = SettingsActivity.PREF_WIND_SPRAY;
-                fanPref = SettingsActivity.PREF_WIND_FAN;
-                colorPref = SettingsActivity.PREF_WIND_COLOR;
-                sprayDefault = SettingsActivity.DEFAULT_WIND_SPRAY;
-                fanDefault = SettingsActivity.DEFAULT_WIND_FAN;
-                colorDefault = SettingsActivity.DEFAULT_WIND_COLOR;
-                break;
-        }
-        int spray = sharedPreferences.getInt(sprayPref, sprayDefault);
-        int fan = sharedPreferences.getInt(fanPref, fanDefault);
-        int color = sharedPreferences.getInt(colorPref, colorDefault);
+        int spray = sharedPreferences.getInt(SettingsActivity.getPrefNameForSprayPeriod(theme),
+                        SettingsActivity.getDefaultForSprayPeriod(theme));
+        int fan = sharedPreferences.getInt(SettingsActivity.getPrefNameForFanSpeed(theme),
+                        SettingsActivity.getDefaultForFanSpeed(theme));
+        int color = sharedPreferences.getInt(SettingsActivity.getPrefNameForColor(theme),
+                        SettingsActivity.getDefaultForColor(theme));
         UDPClientService.sendTheme(MainActivity.this, color, (byte)fan, spray);
     }
 }
--- fancontrol/app/src/main/java/com/idevicesinc/fancontrol/SettingsActivity.java
+++ fancontrol/app/src/main/java/com/idevicesinc/fancontrol/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ import android.widget.SeekBar;
 import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
+    public static final String TAG = "SettingsActivity";
 
     public static final String EXTRA_THEME_ID = "com.idevicesinc.fancontrol.THEME_ID";
     
@@ -38,16 +40,23 @@ public class SettingsActivity extends AppCompatActivity {
     public static final int DEFAULT_WIND_FAN = 50;
     public static final int DEFAULT_WIND_COLOR = 0xFFFFFF;
 
+    private SharedPreferences sharedPreferences;
+    private long fanSpeed;
+    private long sprayPeriod;
+    private long color;
+    private int theme;
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Intent intent = getIntent();
+        theme = intent.getIntExtra(SettingsActivity.EXTRA_THEME_ID, 0);
+
         // set background of activity
         /* MONKEY:
-        Intent intent = getIntent();
-        int theme = intent.getIntExtra(SettingsActivity.EXTRA_THEME_ID, 0);
         Bitmap bitmap = null;
         switch (theme) {
             case R.id.ocean_button:
@@ -68,5 +77,141 @@ public class SettingsActivity extends AppCompatActivity {
         ImageView bgImage = (ImageView) findViewById(R.id.settings_background);
         bgImage.setImageBitmap(newBitmap);
         */
+
+        // initialize widgets based on stored preferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+        sprayPeriod = sharedPreferences.getInt(SettingsActivity.getPrefNameForSprayPeriod(theme),
+                SettingsActivity.getDefaultForSprayPeriod(theme));
+        fanSpeed = sharedPreferences.getInt(SettingsActivity.getPrefNameForFanSpeed(theme),
+                SettingsActivity.getDefaultForFanSpeed(theme));
+        color = sharedPreferences.getInt(SettingsActivity.getPrefNameForColor(theme),
+                SettingsActivity.getDefaultForColor(theme));
+        SeekBar fanBar = (SeekBar) findViewById(R.id.fan_speed);
+        fanBar.setProgress((int)fanSpeed);
+        SeekBar sprayBar = (SeekBar) findViewById(R.id.spray_period);
+        sprayBar.setProgress((int)(256 - sprayPeriod));
+
+        // add listeners to seek bars
+        sprayBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress > 0) {
+                    sprayPeriod = 256 - progress;
+                } else {
+                    sprayPeriod = progress;
+                }
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(TAG, "final spray period: " + sprayPeriod);
+                storeThemeToPreference();
+                sendThemeOverUDP();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+        });
+        fanBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                fanSpeed = progress;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d(TAG, "final fan speed: " + fanSpeed);
+                storeThemeToPreference();
+                sendThemeOverUDP();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    static String getPrefNameForSprayPeriod(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.PREF_OCEAN_SPRAY;
+            case R.id.mountains_button:
+                return SettingsActivity.PREF_MOUNTAINS_SPRAY;
+            case R.id.wind_button:
+                return SettingsActivity.PREF_WIND_SPRAY;
+        }
+        return "";
+    }
+
+    static String getPrefNameForFanSpeed(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.PREF_OCEAN_FAN;
+            case R.id.mountains_button:
+                return SettingsActivity.PREF_MOUNTAINS_FAN;
+            case R.id.wind_button:
+                return SettingsActivity.PREF_WIND_FAN;
+        }
+        return "";
+    }
+
+    static String getPrefNameForColor(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.PREF_OCEAN_COLOR;
+            case R.id.mountains_button:
+                return SettingsActivity.PREF_MOUNTAINS_COLOR;
+            case R.id.wind_button:
+                return SettingsActivity.PREF_WIND_COLOR;
+        }
+        return "";
+    }
+
+    static int getDefaultForSprayPeriod(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.DEFAULT_OCEAN_SPRAY;
+            case R.id.mountains_button:
+                return SettingsActivity.DEFAULT_MOUNTAINS_SPRAY;
+            case R.id.wind_button:
+                return SettingsActivity.DEFAULT_WIND_SPRAY;
+        }
+        return 0;
+    }
+
+    static int getDefaultForFanSpeed(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.DEFAULT_OCEAN_FAN;
+            case R.id.mountains_button:
+                return SettingsActivity.DEFAULT_MOUNTAINS_FAN;
+            case R.id.wind_button:
+                return SettingsActivity.DEFAULT_WIND_FAN;
+        }
+        return 0;
+    }
+
+    static int getDefaultForColor(int theme) {
+        switch (theme) {
+            case R.id.ocean_button:
+                return SettingsActivity.DEFAULT_OCEAN_COLOR;
+            case R.id.mountains_button:
+                return SettingsActivity.DEFAULT_MOUNTAINS_COLOR;
+            case R.id.wind_button:
+                return SettingsActivity.DEFAULT_WIND_COLOR;
+        }
+        return 0;
+    }
+
+    void storeThemeToPreference() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getPrefNameForSprayPeriod(theme), (int)sprayPeriod);
+        editor.putInt(getPrefNameForFanSpeed(theme), (int) fanSpeed);
+        editor.putInt(getPrefNameForColor(theme), (int)color);
+        editor.apply();
+        Log.d(TAG, "storeThemeToPreferences: spray period: " + sprayPeriod + ", fanSpeed: " + fanSpeed + ", color: " + color);
+    }
+
+    void sendThemeOverUDP() {
+        UDPClientService.sendTheme(SettingsActivity.this, color, (byte)fanSpeed, sprayPeriod);
     }
 }
